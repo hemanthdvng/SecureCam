64ef87f80f029e154cf5edd5043bead6c72a694f
hemanthdvng <hemanthdvng1@gmail.com>
2026-03-19 00:13:58 +0530

    Add app/src/main/java/com/securecam/utils/BootReceiver.kt

---
+++ app/src/main/java/com/securecam/utils/BootReceiver.kt
@@ -0,0 +1,3 @@
+package com.securecam.utils
+import android.content.BroadcastReceiver; import android.content.Context; import android.content.Intent
+class BootReceiver : BroadcastReceiver() { override fun onReceive(context: Context, intent: Intent) {} }
