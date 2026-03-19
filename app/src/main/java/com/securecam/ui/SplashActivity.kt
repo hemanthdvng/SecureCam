136e25eb8e97c16f426c53c336ebdb51f7006805
hemanthdvng <hemanthdvng1@gmail.com>
2026-03-19 22:45:24 +0530

    add: app/src/main/java/com/securecam/ui/SplashActivity.kt

---
+++ app/src/main/java/com/securecam/ui/SplashActivity.kt
@@ -0,0 +1,34 @@
+package com.securecam.ui
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.animation.AnimationUtils
+import androidx.appcompat.app.AppCompatActivity
+import com.securecam.R
+import com.securecam.databinding.ActivitySplashBinding
+
+@SuppressLint("CustomSplashScreen")
+class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val fadeIn = AnimationUtils.loadAnimation(this, android.R.anim.fade_in)
+        binding.logoImage.startAnimation(fadeIn)
+        binding.appNameText.startAnimation(fadeIn)
+        binding.taglineText.startAnimation(fadeIn)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this, MainActivity::class.java))
+            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+            finish()
+        }, 2000)
+    }
+}
